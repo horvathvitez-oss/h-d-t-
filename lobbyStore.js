149bdbb35767dc721f84ac83b5dab7436743325d
Vitez <horvathvitez@gmail.com>
2026-03-18 12:27:54 +0100

    update file

--- lobbyStore.js
+++ lobbyStore.js
@@ -1,8 +1,12 @@
+
+
 var lobbies = {};
+
 
 function normalizeGameId(gameid) {
   return String(gameid);
 }
+
 
 function cloneLobby(lobby) {
   return {
@@ -16,6 +20,7 @@ function cloneLobby(lobby) {
   };
 }
 
+
 module.exports.generateLobbyCode = function() {
   var code = '';
   do {
@@ -23,6 +28,7 @@ module.exports.generateLobbyCode = function() {
   } while (lobbies[code]);
   return code;
 };
+
 
 module.exports.createLobby = function(data) {
   var gameid = normalizeGameId(data.gameid);
@@ -38,10 +44,12 @@ module.exports.createLobby = function(data) {
   return cloneLobby(lobbies[gameid]);
 };
 
+
 module.exports.getLobby = function(gameid) {
   var lobby = lobbies[normalizeGameId(gameid)];
   return lobby ? cloneLobby(lobby) : null;
 };
+
 
 module.exports.isParticipant = function(gameid, username) {
   var lobby = lobbies[normalizeGameId(gameid)];
@@ -49,50 +57,62 @@ module.exports.isParticipant = function(gameid, username) {
   return lobby.players.indexOf(username) !== -1;
 };
 
+
 module.exports.joinLobby = function(gameid, username) {
   var key = normalizeGameId(gameid);
   var lobby = lobbies[key];
+
 
   if (!lobby) return { ok: false, reason: 'not_found' };
   if (lobby.started) return { ok: false, reason: 'started' };
   if (lobby.players.indexOf(username) !== -1) return { ok: true, lobby: cloneLobby(lobby) };
   if (lobby.players.length >= lobby.maxPlayers) return { ok: false, reason: 'full' };
 
+
   lobby.players.push(username);
   return { ok: true, lobby: cloneLobby(lobby) };
 };
+
 
 module.exports.leaveLobby = function(gameid, username) {
   var key = normalizeGameId(gameid);
   var lobby = lobbies[key];
 
+
   if (!lobby) return { ok: false, reason: 'not_found' };
   if (lobby.started) return { ok: true, lobby: cloneLobby(lobby), skipped: true };
+
 
   lobby.players = lobby.players.filter(function(playerName) {
     return playerName !== username;
   });
+
 
   if (lobby.players.length === 0) {
     delete lobbies[key];
     return { ok: true, destroyed: true };
   }
 
+
   if (lobby.host === username) {
     delete lobbies[key];
     return { ok: true, destroyed: true, hostLeft: true };
   }
 
+
   return { ok: true, lobby: cloneLobby(lobby) };
 };
+
 
 module.exports.startLobby = function(gameid, username) {
   var key = normalizeGameId(gameid);
   var lobby = lobbies[key];
 
+
   if (!lobby) return { ok: false, reason: 'not_found' };
   if (lobby.host !== username) return { ok: false, reason: 'only_host_can_start' };
   if (lobby.players.length !== lobby.maxPlayers) return { ok: false, reason: 'not_enough_players' };
+
 
   lobby.started = true;
   return { ok: true, lobby: cloneLobby(lobby) };
